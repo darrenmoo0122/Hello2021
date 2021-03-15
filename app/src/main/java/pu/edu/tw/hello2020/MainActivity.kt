8999923d7bc7f7ce501247a8691ba8558d912fac
Darren Moo Wan Sheng <o1083002@gm.pu.edu.tw>
2021-03-15 11:58:56 +0800

    again

--- app/src/main/java/pu/edu/tw/hello2020/MainActivity.kt
+++ app/src/main/java/pu/edu/tw/hello2020/MainActivity.kt
@@ -9,14 +9,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var txv:TextView = findViewById<TextView>(R.id.txv)
-        txv.text = 'Hello Darren'
+        var txv: TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Hello Darren"
+        //TestDEV()
+        //TestMaster()
+        txv.setText("嗨，我是巫萬升");
 
-        TestMaster()
-    }
-
-    fun TestMaster(){
-        var txv:TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Master分支"
     }
 }
